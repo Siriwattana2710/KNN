0adc2f8fd57cdbf0baec1ad416a5095c381ebece
Siriwattana2710 <bam48352@gmail.com>
2025-04-09 13:10:47 +0700

    Update Knn.py

--- Knn.py
+++ Knn.py
@@ -10,15 +10,15 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
    st.header("Versicolor")
-   st.image("./img/ooo.jpg")
+   st.image("./img/iris1.jpg")
 
 with col2:
    st.header("Verginiga")
-   st.image("./img/hhh.jpg")
+   st.image("./img/iris2.jpg")
 
 with col3:
    st.header("Setosa")
-   st.image("./img/kkk.jpg")
+   st.image("./img/iris3.jpg")
 
 html_7 = """
 <div style="background-color:#f7b3f1;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
